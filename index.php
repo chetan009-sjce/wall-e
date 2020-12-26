e335d945e343e86f52ba7a30d1cc885bceefada4
chetan009-sjce <76608950+chetan009-sjce@users.noreply.github.com>
2020-12-26 18:58:54 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!..bootstrap>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!..font awesome>
+    <link rel="stylesheet" href="css/all.min.css">
+    
+     <!..google fonts>
+     <link href="https://fonts.googleapis.com/css2?family=Black+Ops+One&family=Ultra&display=swap" rel="stylesheet">
+
+    <!..custom css>
+    <link rel="stylesheet" href="css/custom.css">
+    <!..style>
+    .active{
+    color:black;
+
+}style>
+
+
+    <title>Wall-e</title>
+</head>
+<body>
+    <!..navigation start>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-danger pl-5 fixed-top">
+        <a href="index.php" class="navbar-brand">Co-Hasna</a>
+        <span class="navbar-text">Let's Spread Some Smile...</span>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#myMenu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="myMenu">
+            <ul class="navbar-nav pl-5 custom-nav">
+                <li class="nav-item"><a href="index.php" class="nav-link active">Home</a></li>
+                <li class="nav-item"><a href="#Services" class="nav-link">Services</a></li>
+                <li class="nav-item"><a href="#Registration" class="nav-link">Registration</a></li>
+                <li class="nav-item"><a href="Requester/RequesterLogin.php" class="nav-link">Login</a></li>
+                <li class="nav-item"><a href="#Contact" class="nav-link">Contact</a></li>
+            
+            
+            </ul>
+
+        </div>
+
+
+    </nav><!..end nav>
+    <!..header jumbottron>
+    <header class="jumbotron backimage" style="background-image:url(images/img2.jpg);">
+     <div class="myclass mainHeading">
+         <h1 class="text-uppercase text-info font-weight-bold">elcome to Co-Hasna</h1>
+         <p class="font-italic text-success">Have your queiries cleared...</p>
+         <a href="Requester/RequesterLogin.php" class="btn btn-success mr-4">Login</a>
+         <a href="#Registration" class="btn btn-danger mr-4">Sign Up</a>
+
+     </div>
+
+</header>
+<!..intro>
+<div class="container">
+    <div class="jumbotron">
+        <h3 class="text-center">Services Info</h3>
+        <p>1.Request or accept an emergency case in which people who require urgent blood plasma of cured patients can request. Those who are willing to give can accept. {We see many Instagram stories and WhatsApp status for asking help for blood plasma, so we thought of why not to make a common platform}.The same procedure is followed where the requisite gives all the necessary details, if by that time in near by vicinity if its available anywhere people can help.
+
+2.An option in which cured patients can give there suggestions here. We are seeing people are dying now due to fear and loss of will power. These feedbacks from cured patients will boost their will power and mental strength, which directly improves their health.
+
+ If people wish to help them they can donate , to their interested association.
+3.link private hospital  and government hospital administrators so that in case of any emergency of plasma this app can notify their hospital or in case if the hospital wants it then the cured patients can get a notification about the same cause. In addition to that, it would be helpful for people who are searching for hospitals which has empty beds, this app serves this purpose spot on as real time data of number of ICU beds available will be displayed in our website.
+Thank you  </p>
+    
+    </div>
+
+</div><!..end intro>
+
+<!..sevices>
+<div class="container text-center border-bottom" id="Services">
+    <h2>Our Services</h2>
+    <div class="row mt-4">
+        <div class="col-sm-4 mb-4">
+            <a href="#"><i class="fas fa-hospital-alt fa-8x text-success"></i></a>
+            <h4 class="mt-4">Hospitalization</h4>
+        </div>
+        <div class="col-sm-4 mb-4">
+            <a href="#"><i class="fas fa-virus-slash fa-8x text-primary"></i></a>
+            <h4 class="mt-4">Preventive Maintenance</h4>
+        </div>
+        <div class="col-sm-4 mb-4">
+            <a href="#"><i class="fas fa-user-md fa-8x text-info"></i></a>
+            <h4 class="mt-4">Guidence</h4>
+        </div>
+    </div>
+</div><!..end service>
+
+<!-- Start Registration Form -->
+<?php include('userRegistration.php') ?>
+<!-- End Registration Form Container -->
+
+<!-- Start Happy Customer -->
+  <div class="jumbotron bg-danger">
+        <div class="container">
+            <h2 class="text-center text-white">Happy Patients Cured</h2>
+            <div class="row mt-5">
+                <div class="col-lg-3 col-sm-6"> <!-- Start 1st Column -->
+                    <div class="card shadow-lg mb-2"> 
+                      <div class="card-body" text-center>
+                        <img src="            " class="img-fluid"
+                        style="border-radius: 100px;"alt="avt1">
+                        <h4 class="card-title">Astroaddy</h4>
+                        <p class="card-text">ohh yea !! the best service i ever had
+                        in my life , it wa better than i expected. thank you so much XD.</p>
+                      </div>  
+                    </div>       
+                
+            
+        
+                 </div><!-- End 1st Column -->
+<div class="col-lg-3 col-sm-6"> <!-- Start 2nd Column -->
+      <div class="card shadow-lg mb-2"> 
+        <div class="card-body" text-center>
+           <img src="            " class="img-fluid"
+           style="border-radius: 100px;"alt="avt1">
+           <h4 class="card-title">Vinutha_h_s</h4>
+           <p class="card-text">ohh yea !! the best service i ever had
+               in my life , it was better than i expected. thank you so much XD.</p>
+       </div>       
+    </div>
+</div>   <!-- End 2nd Column -->
+<div class="col-lg-3 col-sm-6"> <!-- Start 3rd Column -->
+      <div class="card shadow-lg mb-2"> 
+            <div class="card-body" text-center>
+                <img src="            " class="img-fluid"
+                style="border-radius: 100px;"alt="avt1">
+                <h4 class="card-title">Tashuu</h4>
+                <p class="card-text">ohh yea really a good service
+               in my life , it was better than i expected. thank you so much XD.</p>
+            </div>       
+       </div>
+    </div>   <!-- End 3rd Column -->
+<div class="col-lg-3 col-sm-6"> <!-- Start 4th Column -->
+      <div class="card shadow-lg mb-2"> 
+            <div class="card-body" text-center>
+                <img src="            " class="img-fluid"
+                style="border-radius: 100px;"alt="avt1">
+                <h4 class="card-title">Neha_jadhav</h4>
+                <p class="card-text">ohh yea !! the best service i ever had
+                    in my life , it wa better than i expected. thank you so much XD.</p>
+            </div>       
+        </div>
+    </div>   <!-- End 4th Column -->
+</div>  
+  </div>
+</div>      <!-- End Happy Customer -->
+
+<!-- Start Contact US -->
+<div class="container" id="Contact">
+    <h2 class="text-center mb-4">Contact Us</h2>
+    <div class="row">
+         <!-- start 1st column -->
+         <?php include('contact.php') ?>
+         <!-- End 1st column -->
+            <div class="col-md-4 text-center"> <!-- start second column -->
+               <strong>Headquarter:</strong><br>
+                Banashankri Road,<br> 
+                4th main Devanahalli.<br>
+                Banglore-#009-898 <br>
+                <a href="#" target="_blank">www.cohasna.com</a><br>
+                <br><br>
+                <strong>Branch</strong><br>
+                Allok Nagar,<br> 
+                Panaji City Center.<br>
+                Beach Road<br>
+                Goa-#909-600<br>
+               
+                <a href="#" target="_blank">www.cohasna.com</a><br>
+         </div> <!-- End 2nd Column -->
+     </div>
+ </div> <!-- End contact Us -->
+
+<!-- start footer -->
+
+<footer class="container-fluid bg-dark mt-5 text-white">
+    <div class="container">
+        <div class="row py-3">
+            <div class="col-md-6"> <!-- start 1st column -->
+              <span class="pr-2">Follow Us:</span>
+              <a href="https://m.facebook.com/chetanreddy.c.kadda" target="_blank" class="pr-2 fi-color"><i
+              class="fab fa-facebook-f"></i></a>
+              <a href="#" target="_blank" class="pr-2 fi-color"><i
+              class="fab fa-twitter"></i></a>
+              <a href="https://www.instagram.com/invites/contact/?i=1l29peha20gmr&utm_content=7ez21f8" target="_blank" class="pr-2 fi-color"><i
+              class="fab fa-instagram"></i></a>
+              <a href="#" target="_blank" class="pr-2 fi-color"><i
+              class="fab fa-google-plus-g"></i></a>
+              <a href="#" target="_blank" class="pr-2 fi-color"><i
+              class="fab fa-rss"></i></a>
+             
+            </div><!-- end of 1st column -->
+              <div class="col-md-6 text-right"> <!-- Start 2nd Column -->
+                <small>Designed by Squad.007[Bobby] &copy; 2020</small>
+                <small class="ml-2"><a href="admin/login.php">Admin Login</a></small>
+            </div> <!-- End 2nd Column -->
+     </div>
+    </div>
+</footer>
+
+
+    <!..js>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/all.min.js"></script>
+
+    
+</body>
+</html>
